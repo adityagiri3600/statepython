cc5c83dceba1446bc20431d7548adccea76a8849
Aditya Giri <74224708+adityagiri3600@users.noreply.github.com>
2025-01-25 17:20:18 +0530

    update: project structure

--- api/main.py
+++ api/main.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 import sys
 from io import StringIO
 
-app = Flask(__name__, static_folder='build', static_url_path='')
+app = Flask(__name__, static_folder='../frontend/build', static_url_path='')
 CORS(app)
 
 @app.route('/')
